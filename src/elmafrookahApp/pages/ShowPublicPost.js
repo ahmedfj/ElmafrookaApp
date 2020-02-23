3ed3f4b8edf9597f9e09d82b9f5dcd984791f40f
ahmedalthagafi <a.althagafi92@gmail.com>
2020-02-23 17:48:16 +0300

    make it better

--- src/elmafrookahApp/pages/ShowPublicPost.js
+++ src/elmafrookahApp/pages/ShowPublicPost.js
@@ -6,15 +6,16 @@ import longInterVIcon from "../../images/leftarrow.png";
 import shortInterVIcon from "../../images/rightarrow.png";
 import reviewsIcon from "../../images/reviewsicon.png";
 import profileIcon from "../../images/profileicon1.png";
-import ReactHtmlParser from "react-html-parser";
+import ReactHtmlParser from 'react-html-parser';
 
 import logo from "../../images/logo/logo.png";
 import blankImg from "../../images/blank.png";
 
 import "./Pages.scss";
 import ReactPlayer from "react-player";
-import ScreenLoader from "../components/ScreenLoader";
+import ScreenLoader from "../components/ScreenLoader"
 import RatingStars from "../components/RatingStars";
+
 
 const ShowPublicPost = ({ postId, name }) => {
   const [post, setPost] = useState({});
@@ -26,7 +27,7 @@ const ShowPublicPost = ({ postId, name }) => {
       .then(res => {
         setPost(res.data.post);
         setTimeout(() => {
-          setLoader(false);
+          setLoader(false) 
         }, 1000);
       })
       .catch(err => console.log(err));
@@ -34,37 +35,29 @@ const ShowPublicPost = ({ postId, name }) => {
   const vidPlayer = (
     <div>
       <div className="player-wrapper">
-        <ReactPlayer
-          config={{
-            youtube: {
-              playerVars: { controls: 1 }
-            }
-          }}
-          width="100%"
-          height="100%"
-          url={post.videolink}
-        />
+        <ReactPlayer  config={{
+    youtube: {
+      playerVars: { controls: 1 }
+    }
+  }} width="100%" height="100%" url={post.videolink} />
       </div>
+  
     </div>
   );
 
-  return isLoading ? (
-    <ScreenLoader />
-  ) : (
-    <div id="post-page">
+  return (
+  isLoading ? <ScreenLoader/> : <div id="post-page">
       <hr />
       <div className="page-title">
-        {post.typeofpost === "مقابلات" ? (
-          <p style={{ color: "rgb(61, 127, 179)" }}>مقـــــــابــــــلات</p>
-        ) : post.typeofpost === "فيتشر" ? (
-          <p style={{ color: "rgb(61, 127, 179)" }}>فيــــتــــشـر</p>
-        ) : post.typeofpost === "مراجعات" ? (
-          <p style={{ color: "rgb(61, 127, 179)" }}>مــــراجعــــات</p>
-        ) : post.typeofpost === "بروفايل" ? (
-          <p style={{ color: "rgb(61, 127, 179)" }}>بروفــــــايـل</p>
-        ) : (
-          ""
-        )}
+    
+          {  post.typeofpost === "مقابلات"
+                ? <p style={{color: "rgb(61, 127, 179)"}}>مقـــــــابــــــلات</p>
+                : post.typeofpost === "فيتشر"
+                ? <p style={{color:  "rgb(61, 127, 179)"}}>فيــــتــــشـر</p>
+                : post.typeofpost === "مراجعات"
+                ? <p style={{color:  "rgb(61, 127, 179)"}}>مــــراجعــــات</p>
+                : post.typeofpost === "بروفايل"
+                ? <p style={{color:  "rgb(61, 127, 179)"}}>بروفــــــايـل</p> : ""}
         <img
           className=""
           src={
@@ -74,91 +67,74 @@ const ShowPublicPost = ({ postId, name }) => {
               ? shortInterVIcon
               : post.typeofpost === "مراجعات"
               ? reviewsIcon
-              : post.typeofpost === "بروفايل"
-              ? profileIcon
-              : ""
+             : post.typeofpost === "بروفايل"
+              ? profileIcon : ""
           }
           alt="news-icon"
         />
       </div>
-
+     
       <hr />
       <div className="postBody">
         <div className="title-rate">
           <h3
             style={{
-              color: "rgb(61, 127, 179)"
-            }}
+              color:"rgb(61, 127, 179)"}}
           >
             {post.title}
           </h3>
         </div>
-        {!post.videolink && !post.imge ? (
-          <img className="postImage-logo" src={logo} alt="post-img" />
-        ) : (
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center"
-            }}
-          >
-            {post.vidorimg === "صورة" ? (
-              post.typeofpost === "بروفايل" ? (
-                <div className="postImage-box-profile">
-                  <img className="postImage" src={post.imge} alt="post-img" />
-                </div>
-              ) : (
-                <div className="postImage-box">
-                  <img className="postImage" src={post.imge} alt="post-img" />
-                </div>
-              )
-            ) : post.vidorimg === "فديو" ? (
-              vidPlayer
-            ) : (
-              ""
-            )}
-          </div>
-        )}
-
-        <div className="m-title-rate">
-          <h3 style={{ color: "rgb(61, 127, 179)" }}>{post.title}</h3>
+       {!post.videolink && !post.imge ?
+       <img className="postImage-logo" src={logo} alt="post-img" />
+        :  
+     <div style={{width: "100%",
+           display: "flex", flexDirection:"column",alignItems:"center"}}>
+           {post.vidorimg === "صورة" ? (
+         post.typeofpost === "بروفايل" ? <div className="postImage-box-profile">
+    <img className="postImage" src={post.imge}  alt="post-img"/>
+             </div> : <div className="postImage-box">
+    <img className="postImage" src={post.imge}  alt="post-img"/>
+             </div> 
+       
+        ) : post.vidorimg === "فديو" ? (
+          vidPlayer
+        ) : ""}
         </div>
+     
+     } 
+       
+        <div className="m-title-rate">
+          <h3 style={{color:"rgb(61, 127, 179)"}}>
+            {post.title}
+          </h3>
+        </div>
+        
+        {post.writername ?
+            
+            <div className="writer-box">
+              <div className="img-box">
+              <img className="writerimg"  src={post.writerimge ? post.writerimge : blankImg} alt="item" />
 
-        {post.writername ? (
-          <div className="writer-box">
-            <div className="img-box">
-              <img
-                className="writerimg"
-                src={post.writerimge ? post.writerimge : blankImg}
-                alt="item"
-              />
-            </div>
+              </div>
             <div>
-              <p style={{ fontSize: "17px", padding: "0" }}>
-                <span>الكاتب :</span>
-                {post.writername}
-              </p>
-              {post.typeofpost === "بروفايل" ? (
-                ""
-              ) : (
-                <RatingStars rating={post.rating} />
-              )}
+            <p style={{fontSize:"17px", padding:"0"}}><span>الكاتب :</span>{post.writername}</p>
+            {post.typeofpost === "بروفايل" ? "" : <RatingStars  rating={post.rating}/>}
             </div>
-          </div>
-        ) : (
-          ""
-        )}
+           
+            </div>
+     
 
+        
+          :""}
+          
         <hr />
         <span>
           {new Date(`${post.createdAt}`).toLocaleDateString("ar-MA", options)}
         </span>
-        <div style={{ color: post.description_color, width: "100%" }}>
-          {post.description}
-        </div>
-
+        <div style={{color:post.description_color,width: "100%"}}>
+              {ReactHtmlParser(post.description)} 
+             </div>
+         
         {console.log(post.description)}
       </div>
     </div>
